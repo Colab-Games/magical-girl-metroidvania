94b0cd9cabcd59a7f859ac86d107eb4538fcc55b
Igor Cescon de Moura <igorcmgamer@gmail.com>
2021-02-23 22:39:08 -0300

    Add character controller

---
+++ Assets/Scripts/Vector2Extension.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Math2D
+{
+    public static class Vector2Extension
+    {
+        public static Vector3 ToVector3OnXY(this Vector2 v2, float z)
+        {
+            return new Vector3(v2.x, v2.y, z);
+        }
+
+        public static Vector3 ToVector3OnXZ(this Vector2 v2, float y)
+        {
+            return new Vector3(v2.x, y, v2.y);
+        }
+
+        public static Vector3 ToVector3OnYZ(this Vector2 v2, float x)
+        {
+            return new Vector3(x, v2.x, v2.y);
+        }
+    }
+}
