e79d61484749cdf3d709dd8b250bdc006f664825
luisberns <louisuntitled@gmail.com>
2021-03-01 22:21:24 -0300

    Added new physics, mechanics and player animations

--- Assets/Scripts/CharacterController/CharacterController2D.cs
+++ Assets/Scripts/CharacterController/CharacterController2D.cs
@@ -1,180 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
 using Math2D;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
-public class CharacterController2D : MonoBehaviour
+public class CharacterController2D : MonoBehaviour 
 {
-    [Header("Movement Properties")]
-    public float speed = 2f;
-    public float jumpVelocity = 10f;
-    public float airHorizontalInertia = 5f;
-    public float wallJumpVelocity = 5f;
-    
 
     [Header("Physics Check")]
-    public float groundDistance = 0.1f;
-    public float feetDistance = 0.9f;
-    public float wallDistance = 0.1f;
-    public float wallGrabHeight = 1f;
+    public float gravityModifier = 1f;
+    public float minGroundNormalY = .65f;
 
-    Rigidbody2D _rb;
-    BoxCollider2D _bc;
-    Animator _anim;
-    int _collisionLayerMask;
+    protected Vector2 _targetVelocity;
+    protected bool _grounded;
+    protected Vector2 _groundNormal;
+    protected Rigidbody2D _rb;
+    protected ContactFilter2D _contactFilter;
+    protected Vector2 _velocity;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D>hitBufferList = new List<RaycastHit2D>(16);
+    protected Animator _anim;
 
-    bool _jumpPressed = false;
 
-    // Physics checks
-    bool _isGrounded = false;
-    bool _isTouchingWallFront = false;
-    bool _isTouchingWallBack = false;
+    protected const float MinMoveDistance = 0.01f;
+    protected const float shellRadius = 0.01f;
 
-    float _originalGravity;
-    float _originalXScale;
-    int _direction = 1;
-
-    void Start()
+    void OnEnable() 
     {
         _rb = GetComponent<Rigidbody2D>();
-        _bc = GetComponent<BoxCollider2D>();
         _anim = GetComponent<Animator>();
-
-        _collisionLayerMask = Physics2D.GetLayerCollisionMask(gameObject.layer);
-
-        _originalGravity = _rb.gravityScale;
-        _originalXScale = transform.localScale.x;
     }
 
-    void Update()
+    void Start() 
     {
-        if (Input.GetButtonDown("Jump")) {
-            _jumpPressed = true;
-        }
+        _contactFilter.useTriggers = false;
+        _contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask (gameObject.layer));
     }
 
-    void FixedUpdate()
+    void Update() 
     {
-        PhysicsChecks();
-
-        float horizontal = Input.GetAxis("Horizontal");
-        float xVelocity = horizontal * speed;
-
-        float yVelocity = _rb.velocity.y;
-
-        // Animation
-        bool isMoving = xVelocity != 0;
-        if (!isMoving) {
-            _anim.SetBool("isRunning", false);
-        } else {
-            _anim.SetBool("isRunning", true);
-        }
-
-        bool isFalling = yVelocity < -0.2;
-        if (isFalling) {
-            _anim.SetBool("isFalling", true);
-        } else {
-            _anim.SetBool("isFalling", false);
-        }
-
-        bool isJumping = yVelocity > 0.7 && !_isGrounded;
-        if (isJumping) {
-            _anim.SetBool("isJumping", true);
-        } else {
-            _anim.SetBool("isJumping", false);
-        }
-
-        // Wall grab
-        bool canGrab = yVelocity <= 0f && !_isGrounded && _isTouchingWallFront;
-        if (Mathf.Abs(xVelocity) > 0f && canGrab) {
-            yVelocity = 0;
-            _rb.gravityScale = 0f;
-        } else
-            _rb.gravityScale = _originalGravity;
-
-        // Air horizontal movement
-        if (!_isGrounded) {
-            float keptVelocity = _rb.velocity.x;
-            
-            if (Mathf.Abs(keptVelocity) > 0 || Mathf.Abs(xVelocity) > 0) {
-                xVelocity = Mathf.Clamp(keptVelocity + ((xVelocity - _direction) / airHorizontalInertia), -speed, speed);
-            }
-        } else {
-            _anim.SetBool("isFalling", false);
-        }
-
-        // Jump
-        bool canJump = _isGrounded;
-        if (_jumpPressed && canJump) {
-            yVelocity += jumpVelocity;
-        } else if (!_jumpPressed && _isGrounded){
-            _anim.SetBool("isJumping", false);
-        }
-
-
-        // Wall Jump
-        bool canWallJump = !_isGrounded && (_isTouchingWallFront || _isTouchingWallBack);
-        if (_jumpPressed && canWallJump) {
-            xVelocity = speed * (_isTouchingWallBack ? _direction : -_direction);
-            yVelocity = wallJumpVelocity;
-        }
-        _jumpPressed = false;
-
-
-
-        if (xVelocity * _direction < 0f)
-            FlipCharacterDirection();
-        _rb.velocity = new Vector2(xVelocity, yVelocity);
+        _targetVelocity = Vector2.zero;
+        ComputeVelocity();
     }
 
-    void PhysicsChecks()
+    protected virtual void ComputeVelocity()
     {
-        CheckGrounded();
-        CheckTouchingWalls();
-        // touching ceiling
+
     }
 
-    void CheckGrounded()
+    void FixedUpdate() 
     {
-        // Check collision creating a box from the bottom of the character to groundDistance with width feetDistance
-        Vector2 bottom = transform.position.ToVector2OnXY() + _bc.offset + (Vector2.down * _bc.size.y / 2);
-        Vector2 floorOrigin = bottom + Vector2.down * (groundDistance / 2);
-        Vector2 boxSize = new Vector2(feetDistance, groundDistance);
-        Collider2D collision = Physics2D.OverlapBox(floorOrigin, boxSize, 0, _collisionLayerMask);
+        _velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        _velocity.x = _targetVelocity.x;
 
-        // If collided with a valid collider then is grounded
-        _isGrounded = collision != null;
-        DrawDebug.DrawBox2D(floorOrigin.ToVector3OnXY(transform.position.z), boxSize, _isGrounded ? Color.red : Color.green);
+        _grounded = false;
+
+        Vector2 deltaPosition = _velocity * Time.deltaTime;
+
+        Vector2 moveAlongGround = new Vector2(_groundNormal.y, -_groundNormal.x);
+
+        Vector2 move = moveAlongGround * deltaPosition.x;
+        Movement (move, false);
+
+        move = Vector2.up * deltaPosition.y;
+        Movement (move, true);
     }
 
-    void CheckTouchingWalls()
+    public void Movement(Vector2 move, bool yMovement)
     {
-        Vector2 bottom = transform.position.ToVector2OnXY() + _bc.offset + (Vector2.down * _bc.size.y / 2);
-        Vector2 boxSize = new Vector2(wallDistance, wallGrabHeight);
+        float distance = move.magnitude;
 
-        bool CheckWallCollision(Vector2 checkDirection)
+        if (distance > MinMoveDistance) 
         {
-            Vector2 bottomDirection = bottom + (checkDirection * _bc.size.x / 2);
-            Vector2 directedOrigin = bottomDirection + (Vector2.up * wallGrabHeight / 2) + (checkDirection * wallDistance / 2);
-            Collider2D collision = Physics2D.OverlapBox(directedOrigin, boxSize, 0, _collisionLayerMask);
+            int count = _rb.Cast(move, _contactFilter, hitBuffer, distance + shellRadius);
 
-            bool collided = collision != null;
-            DrawDebug.DrawBox2D(directedOrigin.ToVector3OnXY(transform.position.z), boxSize, collided ? Color.red : Color.green);
-            return collided;
+            hitBufferList.Clear();
+            for (int i = 0; i < count; i++) 
+            {
+                hitBufferList.Add (hitBuffer[i]);
+            }
+
+            for (int i = 0; i < hitBufferList.Count; i++) 
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                
+                if(currentNormal.y > minGroundNormalY) 
+                {
+                    _grounded = true;
+
+                    if (yMovement) 
+                    {
+                        _groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(_velocity, currentNormal);
+                if(projection < 0) 
+                {
+                    _velocity = _velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
         }
 
-        _isTouchingWallFront = CheckWallCollision(Vector2.right * _direction);
-        _isTouchingWallBack = CheckWallCollision(Vector2.left * _direction);
+        _rb.position = _rb.position + move.normalized * distance;
     }
-
-    void FlipCharacterDirection()
-    {
-        _direction *= -1;
-
-        // Flip the characther by flipping the x scale
-        Vector3 scale = transform.localScale;
-        scale.x = _originalXScale * _direction;
-
-        // Apply the new scale
-        transform.localScale = scale;
-    }
+    
 }
+
---
+++ Assets/Scripts/CharacterController/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : CharacterController2D 
+{
+
+    [Header("Movement Properties")]
+    public float maxSpeed = 7f;
+    public float jumpTakeOffSpeed = 10f;
+    public PlayerInput playerInput;
+    
+    protected float _horizontal;
+    protected float _vertical = 0f;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected override void ComputeVelocity()
+    {   
+        Vector2 move = Vector2.zero;
+
+        move.x = _horizontal;
+
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if(flipSprite) 
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        _anim.SetBool("isGrounded", _grounded);
+        _anim.SetFloat("xVelocity", Mathf.Abs(_velocity.x) / maxSpeed);
+        _anim.SetFloat("yVelocity", _velocity.y / maxSpeed);
+
+        _targetVelocity = move * maxSpeed;
+    }
+    public void Move(InputAction.CallbackContext context) 
+    {
+        // Set character velocity and trigger animation
+        _horizontal = context.ReadValue<Vector2>().x;
+    }
+    public void Jump(InputAction.CallbackContext context) 
+    {
+        if (context.performed)
+        {
+            _velocity.y = jumpTakeOffSpeed;
+        }
+
+        if (context.canceled) 
+        {
+            if(_velocity.y > 0) 
+            {
+                _velocity.y = _velocity.y * .5f;
+            }
+        } 
+    }
+    
+}
+
